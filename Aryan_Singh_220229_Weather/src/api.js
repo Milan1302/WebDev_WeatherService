5f0d034e552e1630018daae4b05c39b749950d1d
Aryan Singh <121276389+LongClawSword@users.noreply.github.com>
2024-04-06 17:34:15 +0530

    Add files via upload

---
+++ Aryan_Singh_220229_Weather/src/api.js
@@ -0,0 +1,10 @@
+export const weatherapioptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2ca1ff17e5mshb4b6472c2726b28p1ccbe1jsnc6eebd4662da',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.weatherapi.com/v1';
+export const API_KEY = '65da7cce0bca4fa8bbb45239240604'
